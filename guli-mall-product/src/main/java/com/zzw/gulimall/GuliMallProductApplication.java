5bfd75ef263613f7306d612895174ea734be3b2c
zzw <1351450816@qq.com>
2024-02-05 23:18:30 +0800

    初始化

---
+++ guli-mall-product/src/main/java/com/zzw/gulimall/GuliMallProductApplication.java
@@ -0,0 +1,13 @@
+package com.zzw.gulimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliMallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliMallProductApplication.class, args);
+    }
+
+}
